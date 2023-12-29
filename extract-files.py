cd27c38ffb11ada33a826b26d551f002a94d403d
LuK1337 <priv.luk@gmail.com>
2023-12-29 09:59:31 +0100

    ibiza: Patch configs to drop NFC logging levels
    Co-authored-by: Georg Veichtlbauer <georg@vware.at>
    Change-Id: I42bcd622dea35dd69f4fc6bb72a4767ca69af5c3
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- extract-files.py
+++ extract-files.py
@@ -30,6 +30,10 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/libnfc-nci.conf': blob_fixup()
+        .regex_replace('NFC_DEBUG_ENABLED=0x01', 'NFC_DEBUG_ENABLED=0'),
+    'vendor/etc/libnfc-nxp.conf': blob_fixup()
+        .regex_replace('(NXPLOG_.*_LOGLEVEL)=0x03', '\\1=0x02'),
     (
         'vendor/lib64/camera/components/com.qti.node.dewarp.so',
         'vendor/lib64/camera/components/com.vidhance.node.ica.so',
