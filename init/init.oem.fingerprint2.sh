912f9e7a3d666aef20ce9d2a1970a7e25bc73b18
Onelots <onelots@onelots.fr>
2025-12-14 22:32:14 +0100

    ibiza: init: Selectively update from stock
    Change-Id: I367b66145a413f853a1d4342cd0c1dffd90ba64c

--- init/init.oem.fingerprint2.sh
+++ init/init.oem.fingerprint2.sh
@@ -27,10 +27,11 @@ GKI_PATH=$(getprop $PROP_GKI_PATH)
 # hal_list: the array contains the hal service name.
 #
 # note: all arrays should have the same size.
-vendor_list=('chipone' 'fpc' 'goodix')
-kernel_so_list=("/vendor/lib/modules/$GKI_PATH/fpsensor_spi_tee.ko" "/vendor/lib/modules/$GKI_PATH/fpc1020_mmi.ko" "/vendor/lib/modules/$GKI_PATH/goodix_fod_mmi.ko")
-kernel_so_name_list=("fpsensor_spi_tee.ko" "fpc1020_mmi.ko" "goodix_fod_mmi.ko")
-hal_list=('chipone_fp_hal' 'fps_hal' 'goodix_hal')
+vendor_list=('chipone' 'fpc')
+kernel_so_list=("/vendor/lib/modules/$GKI_PATH/fpsensor_spi_tee.ko" "/vendor/lib/modules/$GKI_PATH/fpc1020_mmi.ko")
+kernel_so_name_list=("fpsensor_spi_tee.ko" "fpc1020_mmi.ko")
+dev_node_list=("/dev/fpsensor" "/sys/class/fingerprint/fpc1020/irq")
+hal_list=('chipone_fp_hal' 'fps_hal')
 last_vendor_index=`expr ${#vendor_list[@]} - 1`
 vendor_list_size=${#vendor_list[@]}
 
@@ -56,7 +57,7 @@ persist_fps_id=/mnt/vendor/persist/fps/vendor_id
 persist_fps_id2=/mnt/vendor/persist/fps/last_vendor_id
 
 FPS_VENDOR_NONE=none
-MAX_TIMES=20
+MAX_TIMES=50
 
 # this property store FPS_STATUS_NONE or FPS_STATUS_OK
 # after start fingerprint hal service, the hal service will set this property.
@@ -90,8 +91,22 @@ function start_hal_service(){
     setprop $prop_fps_ident $FPS_STATUS_NONE
 
     insmod ${kernel_so_list[$1]}
+    log "${kernel_so_list[$1]} insmod"
     sleep 1
     setprop $prop_fps_ident ${vendor_list[$1]}
+
+    retry=1
+    while [ $retry -lt 5 ]
+    do
+        if [ -e  "${dev_node_list[$1]}" ]; then
+            log "dev node : ${dev_node_list[$1]} is ready"
+            break
+        else
+            log "driver is not ready, $retry times wait 1s to retry"
+            sleep 1
+        fi
+        let retry++
+    done
 
     log "start ${hal_list[$1]}"
     start ${hal_list[$1]}
